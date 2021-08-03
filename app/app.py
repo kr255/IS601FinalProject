0c707099626e1b86c582e139ea42a2a7a51b6f5c
kamran raza <kr255@njit.edu>
2021-08-03 13:34:17 -0400

    first route created

--- app/app.py
+++ app/app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, make_response, request, jsonify
 
 
 # Create Flask's `app` object
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def hello():
-    return "Hello World!"
+    if request.method != 'GET':
+        return make_response('Malformed request', 400)
+    my_dict = {'key': 'dictionary value'}
+    headers = {"Content-Type": "application/json"}
+    return make_response(jsonify(my_dict), 200, headers)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
