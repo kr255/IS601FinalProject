7d5682b91deb992708e83549415c9f96ec637424
kamran raza <kr255@njit.edu>
2021-08-03 15:36:36 -0400

    part 3 done

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,7 +1,16 @@
 """App entry point."""
-from app import createApp
+import logging
+import sys
+from finalapp import create_app
 
-app = createApp()
+logger = logging.getLogger()
+logger.level = logging.DEBUG
+stream_handler = logging.StreamHandler(sys.stdout)
+logger.addHandler(stream_handler)
+stream_handler.stream = sys.stdout
+print(sys.path)
 
+app = create_app()
+#
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
